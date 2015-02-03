d1b94f04ae034d52b3ccf8a16c713958d2ce2c07
Stephen Duncan Jr <stephen.duncan@gmail.com>
2015-02-03 13:03:58 -0800

    Add p4merge as difftool and mergetool due to problems with latest diffmerge for Mac

--- fish/oh-my-fish/platforms/Darwin.fish
+++ fish/oh-my-fish/platforms/Darwin.fish
@@ -3,3 +3,5 @@ alias java7 "set -x -g JAVA_HOME (/usr/libexec/java_home -v 1.7)"
 alias java6 "set -x -g JAVA_HOME (/usr/libexec/java_home -v 1.6)"
 
 java8
+
+set -x -g P4MERGE /opt/homebrew-cask/Caskroom/p4merge/2014.2/p4merge.app/Contents/Resources/launchp4merge
