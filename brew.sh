5fc537b0b0356073969cd08507fe26da21554b3c
Stephen Duncan Jr <stephen.duncan@gmail.com>
2015-03-01 13:29:12 -0800

    Add giter8

--- brew.sh
+++ brew.sh
@@ -2,5 +2,5 @@
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install fish brew-cask git python ruby scala sbt
+brew install fish brew-cask git python ruby scala sbt giter8
 brew cask install alfred iterm2 java p4merge spectacle
