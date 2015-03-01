a880e29e3d90050749ea79c09643d38177d49d86
Stephen Duncan Jr <stephen.duncan@gmail.com>
2015-03-01 13:38:30 -0800

    Add sourcetree

--- brew.sh
+++ brew.sh
@@ -3,4 +3,4 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install fish brew-cask git python ruby scala sbt giter8
-brew cask install alfred iterm2 java p4merge spectacle
+brew cask install alfred iterm2 java p4merge spectacle sourcetree
