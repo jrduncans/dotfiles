bc27e2e8a7fc848176b19506df99038c954c7fe3
Stephen Duncan Jr <stephen.duncan@gmail.com>
2015-03-04 16:11:47 -0800

    Add QuickLook plugins

--- brew.sh
+++ brew.sh
@@ -3,4 +3,4 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install fish brew-cask git python ruby scala sbt giter8
-brew cask install alfred iterm2 java p4merge spectacle sourcetree
+brew cask install alfred iterm2 java p4merge spectacle sourcetree qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package
