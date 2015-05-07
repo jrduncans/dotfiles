d79da7f69c8ecef5ddf0675b17507aafddf356e8
Stephen Duncan Jr <stephen.duncan@gmail.com>
2015-05-07 16:58:49 -0700

    Expand brew lists

--- brew.sh
+++ brew.sh
@@ -2,5 +2,5 @@
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install fish brew-cask git python ruby scala sbt giter8 git-extras
-brew cask install alfred iterm2 java p4merge spectacle sourcetree qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package
+brew install fish brew-cask git python ruby scala sbt giter8 git-extras vim maven wget jq dos2unix p7zip unrar tree
+brew cask install alfred iterm2 java spectacle sourcetree qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package vagrant virtualbox p4merge 
