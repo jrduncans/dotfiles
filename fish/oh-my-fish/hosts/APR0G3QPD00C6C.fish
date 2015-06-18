00bed75cb5ecf5b0ba475ede0dac11b0adc5a263
Stephen Duncan Jr <stephen.duncan@gmail.com>
2015-06-17 17:19:38 -0700

    Add some aliases

--- fish/oh-my-fish/hosts/APR0G3QPD00C6C.fish
+++ fish/oh-my-fish/hosts/APR0G3QPD00C6C.fish
@@ -1,3 +1,7 @@
 set -x -g AWS_DEFAULT_PROFILE plus-test-readonly
 set -x -g SQUID_TOOLKIT_USE_SHELL_USER true
 set -x -g SQUID_TOOLKIT_DISABLE_SAVE_CREDENTIALS true
+
+balias get-creds-plus-test-readonly "get_creds.py --account plus-test --email stephen.duncan@nike.com --role readonly"
+balias get-creds-plus-test-poweruser "get_creds.py --account plus-test --email stephen.duncan@nike.com --role poweruser"
+balias get-creds-plus-readonly "get_creds.py --account plus --email stephen.duncan@nike.com --role readonly"
