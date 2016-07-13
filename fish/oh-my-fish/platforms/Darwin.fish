c0c1ed5e293b5a87be17ee5d5495f3a746fe0abb
Stephen Duncan <stephen.duncan@gmail.com>
2016-07-12 17:55:20 -0700

    Updates caskroom path.

--- fish/oh-my-fish/platforms/Darwin.fish
+++ fish/oh-my-fish/platforms/Darwin.fish
@@ -4,4 +4,4 @@ alias java6 "set -x -g JAVA_HOME (/usr/libexec/java_home -v 1.6)"
 
 java8
 
-set -x -g P4MERGE /opt/homebrew-cask/Caskroom/p4merge/2014.3-1007540/p4merge.app/Contents/Resources/launchp4merge
+set -x -g P4MERGE /usr/local/Caskroom/p4merge/2014.3-1007540/p4merge.app/Contents/Resources/launchp4merge
