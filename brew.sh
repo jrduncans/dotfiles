ef8c63f79a0e496b73ebc6d343162a3e1eb5ff90
Stephen Duncan <stephen.duncan@gmail.com>
2017-10-22 16:57:11 -0700

    Fix brew install file

--- brew.sh
+++ brew.sh
@@ -2,5 +2,7 @@
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install fish caskroom/cask/brew-cask git ruby scala sbt giter8 git-extras vim maven wget jq dos2unix p7zip unrar tree awscli coreutils typesafe-activator pigz exa
-brew cask install dropbox 1password alfred iterm2 java spectacle sourcetree dash qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package p4merge font-fira-code
+brew tap caskroom/fonts
+
+brew install fish git ruby scala sbt giter8 git-extras vim maven wget jq dos2unix p7zip unrar tree awscli coreutils typesafe-activator pigz exa
+brew cask install alfred java spectacle sourcetree dash qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package p4merge font-fira-code
