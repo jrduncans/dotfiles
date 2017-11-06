406cdba3d519927b89abc1fbb6db5b74e36fb6bf
Stephen Duncan <stephen.duncan@gmail.com>
2017-11-06 15:28:10 -0800

    Don't reinstall brew; install ripgrep and fd; update ruby prefs in vscode

--- brew.sh
+++ brew.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+type brew 1>/dev/null 2>&1 || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew tap caskroom/fonts
 
-brew install fish git ruby scala sbt giter8 git-extras vim maven wget jq dos2unix p7zip unrar tree awscli coreutils typesafe-activator pigz exa
+brew install fish git ruby scala sbt giter8 git-extras vim maven wget jq dos2unix p7zip unrar tree awscli coreutils typesafe-activator pigz exa ripgrep fd
 brew cask install alfred java spectacle sourcetree dash qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package p4merge font-fira-code
