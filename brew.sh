5c236373d58cdae8818eaf809ddf41f98f8378f1
Stephen Duncan <stephen.duncan@gmail.com>
2017-12-09 20:28:54 -0800

    Add pup to brew install list

--- brew.sh
+++ brew.sh
@@ -4,5 +4,5 @@ type brew 1>/dev/null 2>&1 || ruby -e "$(curl -fsSL https://raw.githubuserconten
 
 brew tap caskroom/fonts
 
-brew install fish git ruby scala sbt giter8 git-extras vim maven wget jq dos2unix p7zip unrar tree awscli coreutils typesafe-activator pigz exa ripgrep fd
+brew install fish git ruby scala sbt giter8 git-extras vim maven wget jq dos2unix p7zip unrar tree awscli coreutils typesafe-activator pigz exa ripgrep fd pup
 brew cask install alfred java spectacle sourcetree dash qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package p4merge font-fira-code
