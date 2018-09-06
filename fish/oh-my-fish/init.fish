9b38d13f0969913d9f53a6c3ae4528e7df9ac826
Stephen Duncan <jrduncans@users.noreply.github.com>
2018-09-06 11:13:39 -0700

    Add BAT_THEME

--- fish/oh-my-fish/init.fish
+++ fish/oh-my-fish/init.fish
@@ -1,4 +1,5 @@
 set -g theme_color_scheme solarized-light
+set -gx BAT_THEME "Monokai Extended Light"
 set -gx LC_ALL en_US.UTF-8
 set DOTFILES ~/dotfiles
 eval (python -m virtualfish auto_activation)
