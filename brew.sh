e6d2e42071845a352a5c45a02e69abd6e55d5c62
Stephen Duncan <jrduncans@users.noreply.github.com>
2018-09-19 10:20:37 -0700

    Add hyperswitch

--- brew.sh
+++ brew.sh
@@ -6,7 +6,7 @@ brew tap caskroom/fonts
 
 brew cask install java
 brew install fish git ruby scala sbt giter8 git-extras maven wget jq dos2unix p7zip unrar tree awscli coreutils typesafe-activator pigz exa ripgrep fd pup ammonite-repl ansible bat python python3
-brew cask install alfred spectacle sourcetree dash qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzip webpquicklook p4v font-fira-code visual-studio-code bartender docker gitter
+brew cask install alfred spectacle sourcetree dash qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzip webpquicklook p4v font-fira-code visual-studio-code bartender docker gitter hyperswitch
 brew tap caskroom/versions
 brew cask install java8
 brew install vim --with-override-system-vi
