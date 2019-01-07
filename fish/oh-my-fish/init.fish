44baf37114bdeb24402fc4f85746401f52ffa73c
Stephen Duncan <jrduncans@users.noreply.github.com>
2019-01-07 09:01:26 -0800

    Switch to dark themes

--- fish/oh-my-fish/init.fish
+++ fish/oh-my-fish/init.fish
@@ -1,5 +1,5 @@
-set -g theme_color_scheme solarized-light
-set -gx BAT_THEME "Monokai Extended Light"
+set -g theme_color_scheme solarized-dark
+set -gx BAT_THEME "Monokai Extended"
 set -gx LC_ALL en_US.UTF-8
 set DOTFILES ~/dotfiles
 eval (python -m virtualfish auto_activation)
