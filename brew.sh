c18c1dbed6a9fae68c5fd0c1fe17eb5581490557
Stephen Duncan <jrduncans@users.noreply.github.com>
2019-02-04 11:36:52 -0800

    Update ignore file and brew list

--- brew.sh
+++ brew.sh
@@ -5,7 +5,7 @@ type brew 1>/dev/null 2>&1 || ruby -e "$(curl -fsSL https://raw.githubuserconten
 brew tap caskroom/fonts
 
 brew cask install java
-brew install fish git ruby scala sbt giter8 git-extras maven wget jq dos2unix p7zip unrar tree awscli coreutils typesafe-activator pigz exa ripgrep fd pup ansible bat python python3
+brew install fish git ruby rbenv ruby-build scala sbt giter8 git-extras maven wget jq fx dos2unix p7zip unrar tree awscli coreutils typesafe-activator pigz exa ripgrep fd pup ansible bat python python3
 brew cask install alfred spectacle sourcetree dash qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzip webpquicklook p4v font-fira-code visual-studio-code bartender docker gitter hyperswitch
 brew tap caskroom/versions
 brew cask install java8
