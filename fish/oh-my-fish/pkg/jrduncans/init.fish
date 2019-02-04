420b5dc5f1da47a88aff71a6067a0a533b3d1c80
Stephen Duncan <jrduncans@users.noreply.github.com>
2019-02-03 20:41:03 -0800

    Add wrapper for ammonite to use from brew; add rbenv

--- fish/oh-my-fish/pkg/jrduncans/init.fish
+++ fish/oh-my-fish/pkg/jrduncans/init.fish
@@ -10,8 +10,14 @@ if status --is-login
   set PATH $PATH ~/bin ~/.local/bin
 end
 
+status --is-interactive; and source (rbenv init -|psub)
+
 function ll --wraps exa
   exa -lh --git $argv
+end
+
+function amm --description 'Scala REPL'
+  sh -c 'amm "$@"' amm $argv
 end
 
 function amm-cats --wraps amm
