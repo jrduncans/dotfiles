97382e2c88f820a00c154190479270b138f0f604
Stephen Duncan <jrduncans@users.noreply.github.com>
2020-06-22 13:36:22 -0700

    Fix git_cleanup_merged_branches to be based on current branch instead of hard-coded to 'master'

--- fish/functions/git_cleanup_merged_branches.fish
+++ fish/functions/git_cleanup_merged_branches.fish
@@ -1,10 +1,10 @@
 function git_cleanup_merged_branches
-  git branch --merged master | grep -v "master"
+  git branch --merged | grep -v (git branch --show-current)
 
   read -p "echo 'delete all of these branches? (y/N): '" do_delete
 
   if test $do_delete = "y"
     echo "deleting..."
-    git branch --merged master | grep -v "master" | xargs -n 1 git branch -d
+    git branch --merged | grep -v (git branch --show-current) | xargs -n 1 git branch -d
   end
 end
--- sbt/1.0/plugins/plugins.sbt
+++ sbt/1.0/plugins/plugins.sbt
@@ -1,3 +1,2 @@
 addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.9.2")
 addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.3.4")
-addSbtPlugin("ch.epfl.scala" % "sbt-bloop" % "1.3.4")
