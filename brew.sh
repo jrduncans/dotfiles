13c451fdfea07ab189e0a664fe5961b36a5e85ec
Stephen Duncan <jrduncans@users.noreply.github.com>
2020-12-21 10:34:41 -0800

    Add some brew installs

--- brew.sh
+++ brew.sh
@@ -4,7 +4,7 @@
 
 brew tap homebrew/cask-fonts
 
-brew install fish git rbenv wget jq dos2unix awscli coreutils pigz exa ripgrep fd pup bat python3 gawk
+brew install fish git rbenv wget jq dos2unix awscli coreutils pigz exa ripgrep fd pup bat python3 gawk htop httpie
 brew install qlcolorcode qlstephen qlmarkdown quicklook-json p4v font-fira-code gitter
 
 brew install vim --with-override-system-vi
