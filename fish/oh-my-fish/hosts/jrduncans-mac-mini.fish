fd67b0893c66489c4770a999193976fb8e128da0
Stephen Duncan <jrduncans@users.noreply.github.com>
2020-12-28 14:07:54 -0800

    Simplify path setup and put in desired order.

--- fish/oh-my-fish/hosts/jrduncans-mac-mini.fish
+++
@@ -1,7 +0,0 @@
-if status --is-login
-  set PATH /opt/homebrew/bin $PATH
-end
-
-function ibrew --wraps brew
-  arch -x86_64 /usr/local/bin/brew $argv
-end
--- fish/oh-my-fish/pkg/jrduncans/init.fish
+++ fish/oh-my-fish/pkg/jrduncans/init.fish
@@ -6,8 +6,27 @@ function jl --description 'colorized pretty-printed JSON in less'
   jq -C . | less -R $argv;
 end
 
+function __path_prepend
+  set p $argv[1]
+  if test -e $p
+    set PATH $p $PATH
+  end
+end
+
 if status --is-login
-  set PATH $PATH ~/.cargo/bin ~/bin ~/.local/bin ~/Library/Application\ Support/Coursier/bin
+  __path_prepend /opt/homebrew/bin
+  __path_prepend ~/local/bin
+  __path_prepend ~/bin
+  __path_prepend ~/.cargo/bin
+  __path_prepend ~/Library/Application\ Support/Coursier/bin
+end
+
+functions -e __path_prepend
+
+if test -e /opt/homebrew/bin
+  function ibrew --wraps brew
+    arch -x86_64 /usr/local/bin/brew $argv
+  end
 end
 
 status --is-interactive; and source (rbenv init -|psub)
