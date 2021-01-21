12b7fcc94f212083758da25fb756438697effca3
Stephen Duncan <jrduncans@users.noreply.github.com>
2021-01-21 10:25:45 -0800

    Make java functions dynamically

--- fish/oh-my-fish/platforms/Darwin.fish
+++ fish/oh-my-fish/platforms/Darwin.fish
@@ -1,26 +1,14 @@
-function java12
-  set -x -g JAVA_HOME (/usr/libexec/java_home -v 12 ^/dev/null)
+function __java_version_function
+  set jv $argv[1]
+  if /usr/libexec/java_home -v $jv &>/dev/null
+    function "java$jv" --inherit-variable jv
+      set -x -g JAVA_HOME (/usr/libexec/java_home -v $jv 2>/dev/null)
+    end
+  end
 end
 
-function java11
-  set -x -g JAVA_HOME (/usr/libexec/java_home -v 11 ^/dev/null)
-end
-
-function java10
-  set -x -g JAVA_HOME (/usr/libexec/java_home -v 10 ^/dev/null)
-end
-
-function java8
-  set -x -g JAVA_HOME (/usr/libexec/java_home -v 1.8 ^/dev/null)
-end
-
-function java7
-  set -x -g JAVA_HOME (/usr/libexec/java_home -v 1.7 ^/dev/null)
-end
-
-function java6
-  set -x -g JAVA_HOME (/usr/libexec/java_home -v 1.6 ^/dev/null)
-end
+set __java_versions 1.8 11 12 13 14 15
+for v in $__java_versions; __java_version_function $v; end
 
 java11
 
