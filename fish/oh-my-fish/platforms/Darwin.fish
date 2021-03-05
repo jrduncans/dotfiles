e0d18f846b927937ffce46806cf11e98284fd266
Stephen Duncan <jrduncans@users.noreply.github.com>
2021-03-05 09:48:57 -0800

    Remove java functions in favor of using sdkman.

--- fish/oh-my-fish/platforms/Darwin.fish
+++ fish/oh-my-fish/platforms/Darwin.fish
@@ -1,15 +1,1 @@
-function __java_version_function
-  set jv $argv[1]
-  if /usr/libexec/java_home -v $jv &>/dev/null
-    function "java$jv" --inherit-variable jv
-      set -x -g JAVA_HOME (/usr/libexec/java_home -v $jv 2>/dev/null)
-    end
-  end
-end
-
-set __java_versions 1.8 11 12 13 14 15
-for v in $__java_versions; __java_version_function $v; end
-
-java11
-
 set -x -g P4MERGE /Applications/p4merge.app/Contents/Resources/launchp4merge
