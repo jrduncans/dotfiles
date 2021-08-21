6ba6101718784a86c78bd86b871a82e3012795fc
Stephen Duncan <jrduncans@users.noreply.github.com>
2021-08-20 22:02:30 -0700

    Update to brew.sh

--- brew.sh
+++ brew.sh
@@ -2,4 +2,4 @@
 
 which brew > /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install fish git vim rbenv wget jq dos2unix awscli coreutils pigz exa ripgrep fd pup bat bottom dust choose-rust xh sd gh starship coursier plantuml python3 gawk htop p4v font-fira-code font-fira-code-nerd-font jrduncans/timeturner/timeturner
+brew install fish git vim rbenv wget jq dos2unix awscli coreutils pigz exa ripgrep fd pup bat dust choose-rust xh sd gh starship coursier plantuml python3 gawk htop p4v font-fira-code font-fira-code-nerd-font jrduncans/timeturner/timeturner
