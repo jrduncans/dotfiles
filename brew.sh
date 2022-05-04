0728b135db6a96aa507e7b2111cbe29c50cac49b
Stephen Duncan <jrduncans@users.noreply.github.com>
2022-05-03 19:12:06 -0700

    Add 1password-cli

--- brew.sh
+++ brew.sh
@@ -4,3 +4,4 @@ which brew > /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubuserconte
 
 brew tap homebrew/cask-fonts
 brew install fish git git-trim git-delta vim rbenv wget jq dos2unix awscli coreutils pigz exa ripgrep fd pup bat dust choose-rust xh sd gh starship coursier plantuml python3 gawk htop p4v font-fira-code font-fira-code-nerd-font jrduncans/timeturner/timeturner asdf
+brew install --cask 1password/tap/1password-cli
--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,4 @@ test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell
 brew --prefix asdf &> /dev/null; and source (brew --prefix asdf)/libexec/asdf.fish
 test -e {$HOME}/.asdf/plugins/java/set-java-home.fish; and source {$HOME}/.asdf/plugins/java/set-java-home.fish
 which starship > /dev/null; and starship init fish | source
+which op > /dev/null; and op completion fish | source
