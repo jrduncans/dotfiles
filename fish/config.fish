36a5e5d84211b3a3f1079f46cebec60d5a7d8358
Stephen Duncan <jrduncans@users.noreply.github.com>
2024-01-29 09:23:22 -0800

    Misc updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,4 @@
 test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
-brew --prefix asdf &> /dev/null; and source (brew --prefix asdf)/libexec/asdf.fish
-test -e {$HOME}/.asdf/plugins/java/set-java-home.fish; and source {$HOME}/.asdf/plugins/java/set-java-home.fish
 which starship > /dev/null; and starship init fish | source
 which op > /dev/null; and op completion fish | source
+which mise > /dev/null; and mise activate fish | source
