2045540c1043763d07fcc5ece4cb3401578d9d8d
Stephen Duncan <jrduncans@users.noreply.github.com>
2024-04-29 14:58:52 -0700

    Add fzf

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,4 @@
 test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 which starship > /dev/null; and starship init fish | source
 which op > /dev/null; and op completion fish | source
+which fzf > /dev/null; and fzf --fish | source
